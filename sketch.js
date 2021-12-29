80e30207470155dc06c84de5f1e5029c60d55903
arnavkau <94837613+arnavkau@users.noreply.github.com>
2021-12-29 13:15:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,105 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine;
+var world;
+
+var floor;
+var binLeft;
+var binRight;
+var rightEdge;
+var leftEdge;
+
+var ball;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    floor = new Shapes(400, 650, 800, 10)
+	World.add(world, floor)
+
+	binRight = new Shapes(700, 550, 10, 300)
+	World.add(world, binRight)
+
+	binLeft = new Shapes(600, 600, 10, 100)
+	World.add(world, binLeft)
+
+	rightEdge = new Shapes(790, 450, 10, 700)
+	World.add(world, rightEdge)
+
+	leftEdge = new Shapes(10, 450, 10, 700)
+	World.add(world, leftEdge)
+
+    var ball_options = 
+{
+  restitution: 0.95
+}
+
+
+
+
+
+  ball = Bodies.circle(200, 150, 10, ball_options)
+  World.add(world, ball)
+
+
+
+	
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("black");
+
+  Engine.update(engine);
+  floor.display()
+
+  binRight.display()
+  binLeft.display()
+
+  rightEdge.display()
+  leftEdge.display()
+  
+  ellipse(ball.position.x, ball.position.y, 10)
+
+
+  if(keyIsDown(LEFT_ARROW))
+  {
+	  ulForce()
+  }
+
+  if(keyIsDown(RIGHT_ARROW))
+  {
+	  urForce()
+  }
+
+}
+
+function ulForce()
+{
+	Matter.Body.applyForce(ball, {x:ball.position.x, y:ball.position.y}, {x:-2, y:-2})
+}
+
+function urForce()
+{
+	Matter.Body.applyForce(ball, {x:ball.position.x, y:ball.position.y}, {x:2, y:-2})
+}
+
+
+
